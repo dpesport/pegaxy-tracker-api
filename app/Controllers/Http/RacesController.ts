891103f8bc246dd8153f63857d02980b2bb2e781
Alfian Azizi <alfianazizi4869@gmail.com>
2022-02-13 07:37:22 +0700

    handle undefined end_date

--- app/Controllers/Http/RacesController.ts
+++ app/Controllers/Http/RacesController.ts
@@ -12,7 +12,7 @@ export default class RacesController {
     let today = new Date(Date.now())
     let start_date: string = request.qs().start_date
     let end_date: string = request.qs().end_date
-    if (!end_date) end_date = today.toISOString().split('T')[0]
+    if (end_date == undefined) end_date = today.toISOString().split('T')[0]
     let total_days = this.getDifferenceInDays(new Date(end_date), new Date(start_date))
     let total_races = total_days * 24
     let races = await Database.rawQuery("SELECT count(*) as 'total_races',  sum(if(position<=3, 1,0)) as 'win', sum(if(position>3, 1, 0)) as 'lose', sum(if(position<=3, reward, 0)) as 'reward', avg(if(position<=3, 1, 0)) as 'win_rate', count(*)/:total_races as 'pega_usage' FROM races where race_date BETWEEN :start_date and :end_date and pega_id=:pega_id group by pega_id;", {
